bab7ee77b629039043201c8bd37448609587cce5
mhmunter <mark.munter1@gmail.com>
2022-05-14 22:11:48 -0500

    changed password

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -39,73 +39,73 @@ const postdata = [
     user_id: 1,
   },
   {
-    title: "Morbi non quam nec dui luctus rutrum.",
-    comment_text: "http://ucla.edu/consequat/nulla.html",
+    title: "Computers ar the best.",
+    comment_text: "league of legends.",
     user_id: 1,
   },
   {
-    title: "Duis ac nibh.",
-    comment_text: "http://theguardian.com/dui/vel/nisl/duis/ac/nibh.aspx",
+    title: "Game Boy.",
+    comment_text: "the OG, enough said!",
     user_id: 9,
   },
   {
-    title: "Curabitur at ipsum ac tellus semper interdum.",
-    comment_text: "https://reverbnation.com/ligula/sit.jpg",
+    title: "I like books.",
+    comment_text: "The Relic",
     user_id: 5,
   },
   {
-    title: "In hac habitasse platea dictumst.",
-    comment_text: "http://china.com.cn/lectus/vestibulum.json",
+    title: "Sports.",
+    comment_text: "Football.",
     user_id: 3,
   },
   {
-    title: "Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.",
+    title: "TV.",
     comment_text:
-      "http://networksolutions.com/nam/ultrices/libero/non/mattis/pulvinar.json",
+      "Married With Children",
     user_id: 10,
   },
   {
-    title: "Donec dapibus.",
-    comment_text: "https://instagram.com/ac/neque/duis/bibendum/morbi/non.xml",
+    title: "Netflix.",
+    comment_text: "Stragner Things",
     user_id: 8,
   },
   {
-    title: "Nulla tellus.",
-    comment_text: "https://lycos.com/natoque/penatibus/et.html",
+    title: "Paramont.",
+    comment_text: "Halo",
     user_id: 3,
   },
   {
     title:
-      "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.",
-    comment_text: "https://gmpg.org/lorem.jpg",
+      "Crunchy Roll.",
+    comment_text: "That Time I was Reincarnated as a Slime",
     user_id: 3,
   },
   {
     title:
-      "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.",
-    comment_text: "https://paginegialle.it/mattis/egestas.jsp",
+      "Disney.",
+    comment_text: "Mandalorian",
     user_id: 7,
   },
   {
-    title: "In hac habitasse platea dictumst.",
-    comment_text: "http://wikia.com/turpis/eget.jpg",
+    title: "HULU.",
+    comment_text: "Golden Girls",
     user_id: 6,
   },
   {
-    title: "Etiam justo.",
-    comment_text: "https://shareasale.com/quis.json",
+    title: "Pluto.",
+    comment_text: "It's free.",
     user_id: 4,
   },
   {
-    title: "Nulla ut erat id mauris vulputate elementum.",
+    title: "HBO MAX.",
     comment_text:
-      "http://java.com/diam/neque/vestibulum/eget/vulputate/ut/ultrices.png",
+      "Peace Maker",
     user_id: 6,
   },
   {
     title:
-      "Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
-    comment_text: "https://java.com/at/nibh/in.png",
+      "Funimation.",
+    comment_text: "Galatic Heros",
     user_id: 7,
   },
 ];
